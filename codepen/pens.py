329065984452cedc98cc8cf582a6ad663fb500c5
Connor Kendrick <connorkendrickmusic@gmail.com>
2017-10-07 01:42:30 -0400

    Add ability to get pens by tag

--- codepen/pens.py
+++ codepen/pens.py
@@ -18,3 +18,21 @@ class Pens(object):
         path = 'https://cpv2api.com/pens/{}'.format(category)
         response = requests.get(path, params = {'page': str(page)})
         return response.json()['data']
+
+    @staticmethod
+    def tag(tag, page=1):
+        """
+        Get a list of pens with a certain tag.
+
+        Args:
+            tag: The tag to select pens by.
+                (required)
+            page: The page number of the desired data.
+                (default: 1)
+
+        Returns:
+            A list representation of the JSON 'data' key returned from the API.
+        """
+        path = 'https://cpv2api.com/tag/{}'.format(tag)
+        response = requests.get(path, params = {'page': str(page)})
+        return response.json()['data']
--- tests/test_pens.py
+++ tests/test_pens.py
@@ -8,6 +8,7 @@ from codepen import Pens
 Constants
 """
 CATEGORY = 'popular'
+TAG = 'css'
 PAGE = 2
 
 @fixture
@@ -20,6 +21,16 @@ def test_pens_list(pen_keys):
     """Tests an API call to get a list of pens"""
 
     response = Pens.list(CATEGORY, PAGE)
+
+    assert isinstance(response, list), "The response should be a list instance"
+    assert isinstance(response[0], dict), "The response data should be a dict instance"
+    assert set(pen_keys).issubset(response[0].keys()), "All keys should be in the response"
+
+@vcr.use_cassette('vcr_cassettes/pens_tag.yml')
+def test_pens_tag(pen_keys):
+    """Tests an API call to get a list of pens"""
+
+    response = Pens.tag(TAG, PAGE)
 
     assert isinstance(response, list), "The response should be a list instance"
     assert isinstance(response[0], dict), "The response data should be a dict instance"
